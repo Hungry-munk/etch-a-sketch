d6e3acc70b9bd30cb5160b2606c760285e5ffade
Hungry_munk <abbaszadeharsam@gmail.com>
2022-04-11 19:19:56 +1000

    grid has size although I cant get hpver and mouse down to work at the same time

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const gridContainer = document.getElementById('gridContainer')
-
-let sizeOFGrid = 8
+let sizeOFGrid = 6//defult size
+let gridColour = 'black'
 
 function createGrid (size){
+    gridContainer.style.gridTemplateColumns= `repeat(${size}, 1fr)`
+    gridContainer.style.gridTemplateRows= `repeat(${size}, 1fr)`
 
-
-    for (i=0;i<size ** 2; i++){
+    for (i=0; i< size*size; i++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
-        cell.classList.add(`${i+1}`);
-        cell.style.border = 'solid 1px'
-        cell.textContent = 'c'
+        cell.style.border='solid 1px'
         gridContainer.appendChild(cell)
-        gridContainer.style.gridTemplateColumns= `repeat(${size}, 1fr)`
-        gridContainer.style.gridTemplateRows= `repeat(${size}, 1fr)`
-    }
-
+    };
 };
-
 createGrid(sizeOFGrid)
 
-
+gridContainer.addEventListener('mousedown',()=>{
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(Element =>{
+        Element.addEventListener('mouseover',()=>{
+            Element.style = 'background: blue'
+        })
+    }) 
+})

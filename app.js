076c0868c043f1b0addf726d2a5e96de885c66fd
Hungry_munk <abbaszadeharsam@gmail.com>
2022-04-12 11:57:41 +1000

    grids now can do something when mouse is hovering over and mouse button is down

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const gridContainer = document.getElementById('gridContainer')
 let sizeOFGrid = 6//defult size
 let gridColour = 'black'
 
+let mouseDown = false
+document.onmousedown = ()=> mouseDown= true
+document.onmouseup = ()=> mouseDown = false
+
 function createGrid (size){
     gridContainer.style.gridTemplateColumns= `repeat(${size}, 1fr)`
     gridContainer.style.gridTemplateRows= `repeat(${size}, 1fr)`
@@ -9,17 +13,15 @@ function createGrid (size){
     for (i=0; i< size*size; i++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.addEventListener('mouseover' , changeCellColour);
         cell.style.border='solid 1px'
         gridContainer.appendChild(cell)
     };
 };
-createGrid(sizeOFGrid)
 
-gridContainer.addEventListener('mousedown',()=>{
-    const cells = document.querySelectorAll('.cell');
-    cells.forEach(Element =>{
-        Element.addEventListener('mouseover',()=>{
-            Element.style = 'background: blue'
-        })
-    }) 
-})
+function changeCellColour(e){
+    if (e.type == 'mouseover' && !mouseDown ) return //end function right there
+    doucment
+};
+
+createGrid(sizeOFGrid)

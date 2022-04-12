db4894137474f454e28acd5cf0359a9d04a9f1d7
Hungry_munk <abbaszadeharsam@gmail.com>
2022-04-12 13:27:38 +1000

    fixed dragging bug with user-style : none in css

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const gridContainer = document.getElementById('gridContainer')
 let gridSize = 16//defult size
 let gridColour = 'black'
 
+let mouseDown = false;
+gridContainer.onmousedown = ()=>mouseDown= true;//only mousedown on grid
+document.onmouseup = ()=>mouseDown = false;
+
 createGrid(gridSize)//making grid originally
 
-let mouseDown = false
-gridContainer.onmousedown = ()=>mouseDown= true//only mousedown on grid
-document.onmouseup = ()=>{
-    mouseDown = false
-    console.log('up')
-}
 
 function createGrid (size){
     gridContainer.style.gridTemplateColumns= `repeat(${size}, 1fr)`
@@ -18,7 +16,6 @@ function createGrid (size){
     for (let i=0; i< size*size; i++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
-        // cell.draggable = 'false'
         cell.addEventListener('mouseover' , changeCellColour);
         cell.addEventListener('click' , changeCellColour);//so you can click on one square
         cell.style.border='solid 1px'
@@ -28,5 +25,6 @@ function createGrid (size){
 
 function changeCellColour(e){
     if (e.type == 'mouseover' && !mouseDown ) return //end function right there
-    e.target.style = 'background : blue'
+    const cell = e.target //each
+
 };

dee13ebf2430fed0373ab62a3fa0a729e9787e39
Hungry_munk <abbaszadeharsam@gmail.com>
2022-04-14 16:45:33 +1000

    al gird and button functionality is complete

--- app.js
+++ app.js
@@ -1,31 +1,84 @@
 const gridContainer = document.querySelector('.gridContainer')
-let CellColour = document.getElementById('PenColour').value
-let CellBackgroundColour = document.getElementById('backColour').value
-
-let gridSize = document.querySelector('#GridSize').value//showing original size
-const GridSizer = document.querySelector('#GridSize')
 const gridSizeText = document.querySelector('#GridSizeText')
+const GridSizer = document.querySelector('#GridSize')
+const backColour = document.getElementById('backColour')
 
-GridSizer.onmousemove = e=>{//updating the text of slider value
-    gridSize= e.target.value
-    gridSizeText.textContent = `${gridSize} x ${gridSize}`
-};
+//all buttons
+const RainbowBtn = document.getElementById('rainbow')
+const EraserBtn = document.getElementById('eraser')
+const GridBtn = document.getElementById('grid')
+const ClearBtn = document.getElementById('clear')
 
-GridSizer.onchange = e => {//creating a new grid when slider is moved
-    deleteGrid()
-    createGrid(e.target.value)
-}
- 
+//grid colours and size values
+let CellBackgroundColour = document.getElementById('backColour').value
+let gridSize = document.querySelector('#GridSize').value//showing original size
+let CellColour = document.getElementById('PenColour').value
 
+//all different modes
+let RainbowPen = false,
+    Eraser = false,
+    Grid = false;
+
+//wether mouse button is being held down
 let mouseDown = false;//for deciding if mouse button is being held or not
 gridContainer.onmousedown = ()=>mouseDown= true;//only mousedown on grid
 document.onmouseup = ()=>mouseDown = false;
 
-//all different modes
-let ColorPen = true,
-    RainbowPen = false,
-    Eraser = false,
-    Grid = false;
+RainbowBtn.onclick = () =>{
+    if (!RainbowPen){
+        RainbowPen = true
+        Eraser = false
+    }else{
+        RainbowPen = false
+    };
+};
+
+EraserBtn.onclick = () =>{
+    if (!Eraser){
+        Eraser = true
+        RainbowPen = false
+    }else{
+        Eraser = false
+    }
+};
+
+ClearBtn.onclick = () =>{
+    deleteGrid()
+    createGrid(gridSize)
+};
+
+GridBtn.onclick = ()=>{
+    const cells = Array.from(document.getElementsByClassName('cell'))
+
+    cells.forEach(cell =>{
+        cell.classList.toggle('CellGrid')
+    });
+    
+    if (Grid) Grid=false
+    else Grid=true
+};
+
+
+
+//changing canvas bg colour
+backColour.oninput = e =>{//change backgorund colour
+    changeBackColour(e.target.value)
+    CellBackgroundColour = document.getElementById('backColour').value
+};
+
+//changing gridsize text and variable
+GridSizer.onmousemove = e=>{
+    gridSize= e.target.value
+    gridSizeText.textContent = `${gridSize} x ${gridSize}`
+};
+
+//changing grid size
+GridSizer.onchange = e => {//creating a new grid when slider is moved
+    deleteGrid()
+    createGrid(e.target.value)
+};
+
+//functions ---------------
 
 function createGrid (size){
     gridContainer.style.gridTemplateColumns= `repeat(${size}, 1fr)`
@@ -33,12 +86,13 @@ function createGrid (size){
 
     for (let i=0; i< size*size; i++){
         const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.classList.add(`${i+1}`)
-        cell.addEventListener('mouseover' , changeCellColour);
+        cell.classList.add('cell','clean');
+        if (Grid) cell.classList.add('CellGrid')
+        cell.addEventListener('mouseover' , changeCellColour);//so you can drag mouse across grid
         cell.addEventListener('click' , changeCellColour); //so you can click on one square
         cell.style.background = `${CellBackgroundColour}`
-        // cell.style.border = '1px solid'
+        cell.style.border = 1
+
         gridContainer.appendChild(cell)
     };
 };
@@ -46,9 +100,30 @@ function createGrid (size){
 function changeCellColour(e){
     if (e.type == 'mouseover' && !mouseDown ) return //end function right there
     const cell = e.target //indvidual cell in grid
-    CellColour = document.getElementById('PenColour').value
+
+    if (!Eraser){
+    CellColour = RainbowPen?
+        RandomColour() : document.getElementById('PenColour').value
     cell.style = `background-color: ${CellColour}`
+    cell.classList.remove('clean')
+    }else{
+        //checking if cell is already clean
+        const CellClasses = Array.from(cell.classList)//getting all classes of the cell
+        if (CellClasses.includes('clean')) return
+
+        cell.classList.add('clean')
+        cell.style.background = `${CellBackgroundColour}`
+
+    };
 };
+
+function changeBackColour(colour){
+    const cleanCells = Array.from(document.getElementsByClassName('clean'))
+    cleanCells.forEach(cleanCell =>{
+        cleanCell.style = `background-color:${colour}`
+    });
+    CellBackgroundColour = document.getElementById('backColour').value
+}
 
 function deleteGrid(){
     const cells = Array.from(document.getElementsByClassName('cell'))
@@ -56,6 +131,12 @@ function deleteGrid(){
         cell.remove();
     });
 };
+function RandomColour(){
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+};
+
+
+
 
 window.onload = ()=>{
     createGrid(16)//making grid originally
